1d8ef378e582739276a9c9865a7482359b75ead3
Timothee BENHAMOU <timothee.benhamou@estia.local>
2024-02-04 18:35:09 +0100

    travail sur la fenetre pop up pour ajouter des fichiers

--- test_pop_up.js
+++ test_pop_up.js
@@ -2,6 +2,10 @@ function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
   
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
   }
+
+function add_file_window() {
+  document.getElementById("upload_file").style.display = "block";
+}
